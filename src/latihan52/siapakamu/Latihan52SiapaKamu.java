91d02784909711cf6af20fabea99864414e38c30
IRMAN NOVRYANSAH <55683592+IrmanN48@users.noreply.github.com>
2019-11-06 15:32:18 +0700

    Add files via upload

---
+++ src/latihan52/siapakamu/Latihan52SiapaKamu.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan52.siapakamu;
+
+/**
+ *
+ * @author Aero
+ * NAMA  : Irman Novryansah
+ * KELAS : IF 1
+ * NIM   : 10118018
+ * Deskripsi Program : Program ini untuk membuat tampilan dosen dan mahasiswa.
+ */
+public class Latihan52SiapaKamu {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+  Dosen dosen = new Dosen();
+        dosen.setNip("41227829930");
+        System.out.println("NIP DOSEN : "+dosen.getNip());
+        dosen.siapaKamu();
+        dosen.setNama("Rizki Adam Kurniawan");
+        dosen.setUmur(27);
+        dosen.setMataKuliah("PBO");
+        dosen.mengajarApa();
+        
+        Mahasiswa mahasiswa = new Mahasiswa();
+        mahasiswa.setNim("10118018");
+        System.out.println("\nNIM MAHASISWA : "+mahasiswa.getNim());
+        mahasiswa.siapaKamu();
+        mahasiswa.setNama("Irman Novryansah");
+        mahasiswa.setUmur(20);
+        mahasiswa.setKelas("PBO3");
+        mahasiswa.kelasApa();
+    }
+    
+}
